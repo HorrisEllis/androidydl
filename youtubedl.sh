95dce030637a72bfe630e55fcc277f2868a45a76
HorrisEllis <jamesbrooks1234567891011@gmail.com>
2020-10-27 02:52:26 +0000

    Update youtubedl.sh

--- youtubedl.sh
+++ youtubedl.sh
@@ -58,7 +58,7 @@ echo -e $Y $L"Setting up configs..." $N
 sleep 2
 
 echo -e $Y $L "Creating Youtube folder..." $N
-		mkdir /data/data/com.termux/files/home/storage/shared/Youtube
+		mkdir /data/data/com.termux/files/home/storage/shared/Youtubedl
 sleep 1.5
 
 echo -e $Y $L "Creating youtube-dl config..." $N
